61f5a7d695c63da304a65c6bf053059b15ad050e
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-09-12 18:12:54 -0500

    Particles that gravitate towards mouse coordinates

--- master.js
+++ master.js
@@ -1,3 +1,8 @@
+var n = 1000,
+    c = 0,
+    mouse = null,
+    particles = [];
+
 var canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d');
 
@@ -19,13 +24,43 @@ function init() {
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
     ctx.closePath();
 
+    window.onmousemove = function (e) {
+        if (c % 50 === 0) {
+            log('test');
+        }
+
+        mouse = new Vector(e.clientX, e.clientY);
+        c++;
+    }
+
+    for (var i = 0; i < n; i++) {
+        particles.push(new Particle(
+            new Vector(random(WIDTH), random(HEIGHT)),
+            Vector.random()
+        ));
+        particles[i].vel.setMag(random(3, 4));
+    }
+
     ani();
 }
 
 function ani() {
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    for (var i = 0; i < n; i++) {
+        var p = particles[i];
+        p.borders();
+        if (mouse) {
+            var g = Vector.sub(mouse, p.pos);
+            g.setMag(0.05);
+            p.applyForce(g);
+        }
+        p.update();
+        p.show();
+    }
 
 
-    //requestAnimationFrame(ani);
+    requestAnimationFrame(ani);
 }
 
 init();
